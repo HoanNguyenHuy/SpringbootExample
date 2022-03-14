03f954976498c09fe3e1a46ced9227ddb3d41dbd
HoanNguyenHuy <hoannh@vnpt-technology.vn>
2022-03-14 11:12:46 +0700

    init project

---
+++ src/test/java/com/hoannh/tranning01/Tranning01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoannh.tranning01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tranning01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
